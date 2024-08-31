933528f0f44b155f7c3b87f84dec87c7711e32cf
Łukasz Karasiński <lukasz@karasin.ski>
2024-09-01 00:29:14 +0200

    refactor: change tmux theme

--- modules/home/tmux.nix
+++ modules/home/tmux.nix
@@ -19,16 +19,17 @@
 			set -as terminal-overrides ',*:Smulx=\E[4::%p1%dm'
 			set -as terminal-overrides ',*:Setulc=\E[58::2::%p1%{65536}%/%d::%p1%{256}%/%{255}%&%d::%p1%{255}%&%d%;m'
 
+
 			set -g status-position top
-			set -g status-style 'bg=#1e1e2e'
-			set -g message-command-style bg=default,fg=yellow
-			set -g message-style bg=default,fg=yellow
-			set -g mode-style bg=default,fg=yellow
-			set -g status-right '#[fg=#b4befe,bg=#1e1e2e]%d-%m-%Y 󱑒 %H:%M:%S'
+			set -g status-style 'bg=#1F1F28'
+			set -g message-command-style bg=default,fg=#DCD7BA
+			set -g message-style bg=default,fg=#DCD7BA
+			set -g mode-style bg=default,fg=#DCD7BA
+			set -g status-right '#[fg=#7E9CD8,bg=#1F1F28]%d-%m-%Y 󱑒 %H:%M:%S'
 			set -g status-justify absolute-centre
 			set -g status-right-length 200
-			set -g window-status-current-format '#[fg=magenta,bg=#1e1e2e, bold]#I #W'
-			set -g window-status-format '#[fg=gray,bg=#1e1e2e]#I #W'
+			set -g window-status-current-format '#[fg=#957FB8,bg=#1F1F28,bold]#I #W'
+			set -g window-status-format '#[fg=#727169,bg=#1F1F28]#I #W'
 		'';
 	};
 }
