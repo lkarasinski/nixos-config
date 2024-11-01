8eb88d8bcb66bcb66ddd7f1797580918479ae151
Łukasz Karasiński <lukasz@karasin.ski>
2024-11-01 21:56:22 +0100

    feat: add hosts to ssh config

--- modules/home/ssh.nix
+++ modules/home/ssh.nix
@@ -7,6 +7,14 @@
         identityFile = "~/.ssh/id_ed25519";
         user = "lkarasinski";
       };
+      "macbook" = {
+        hostname = "192.168.1.3";
+        user = "karasinski";
+      };
+      "proxmox" = {
+        hostname = "192.168.1.50";
+        user = "lkarasinski";
+      };
     };
   };
 }
