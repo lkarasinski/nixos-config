5b6cd4d8a6f671b56ee6e7a501c9bc2d79f14993
Łukasz Karasiński <lukasz@karasin.ski>
2024-11-02 08:08:18 +0100

    feat: change cursor

--- modules/home/stylix.nix
+++ modules/home/stylix.nix
@@ -5,6 +5,12 @@
   stylix.polarity = "dark";
   stylix.base16Scheme = ./style/kanagawa-dragon.yml;
 
+  stylix.cursor = {
+    name = "Capitaine";
+    size = 32;
+    package = pkgs.capitaine-cursors;
+  };
+
   stylix.fonts = {
     sizes = {
       terminal = 14;
